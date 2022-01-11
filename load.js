ddb47428d1c932e9c9d08a202e9cc6636237482b
elzup <guild0105@gmail.com>
2022-01-11 15:43:41 +0900

    make load.js

--- load.js
+++ load.js
@@ -1,8 +1,15 @@
-function addCss(path) {
+const loadCss = (path) => {
   if (!document) return
   const link = document.createElement('link')
   link.href = path
   link.rel = 'stylesheet'
   link.type = 'text/css'
   document.body.appendChild(link)
+}
+
+const loadJs = (path) => {
+  if (!document) return
+  const el = document.createElement('script')
+  el.src = path
+  document.body.appendChild(el)
 }
